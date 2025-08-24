d63c5bb7a0bda169ad85552fa678db674adb8975
raj05122001 <rajmalviya545@gmail.com>
2025-08-24 13:03:08 +0530

    eco

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "pride_crm_frontend",
+      cwd: "/root/pride_crm_frontend",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3010",
+      env: {
+        NODE_ENV: "production",
+        NEXT_PUBLIC_API_BASE: "https://crm.24x7techelp.com/api/v1"
+      }
+    }
+  ]
+}
+EOF

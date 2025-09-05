d90ec197cde48c4f80c83b7b5a9fc3cce4b9435c
raj05122001 <rajmalviya545@gmail.com>
2025-09-05 16:08:32 +0530

    win7

---
+++ src/app/polyfills.jsx
@@ -0,0 +1,6 @@
+// src/app/polyfills.ts
+import 'core-js/stable';
+import 'whatwg-fetch';
+
+// Optional: URLSearchParams polyfill (rarely needed on old builds)
+// import 'core-js/features/url-search-params';
